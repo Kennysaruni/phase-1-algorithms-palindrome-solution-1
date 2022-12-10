e998d687ba3a23945cdfaf87b640ca2eacde01c6
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-10 17:01:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -9,6 +27,11 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+//an array is initialized to contain the letters of the worf
+// arrays have a built in reverse function in javasript hence the need 
+// once the array is reversed it is joined to a string again
+//isPalindrome checks whether the wo words match 
+//if they do it is a palindrome else it isnt
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
